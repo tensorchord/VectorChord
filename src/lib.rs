c667dceec7ff6430400fe7f861d1ffcfde02ae69
usamoi <usamoi@outlook.com>
2024-11-08 16:28:51 +0800

    debug for laion100m

--- src/lib.rs
+++ src/lib.rs
@@ -19,8 +19,8 @@ pgrx::extension_sql_file!("./sql/finalize.sql", finalize);
 
 #[pgrx::pg_guard]
 unsafe extern "C" fn _PG_init() {
-    if unsafe { pgrx::pg_sys::IsUnderPostmaster } {
-        pgrx::error!("rabbithole must be loaded via shared_preload_libraries.");
+    unsafe {
+        std::env::set_var("RUST_BACKTRACE", "1");
     }
     detect::init();
     unsafe {
