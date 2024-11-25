1d8a9e18f707de1be7939a734339593dacc1ebea
xieydd <xieydd@gmail.com>
2024-11-25 15:31:56 +0800

    fix in comments
    Signed-off-by: xieydd <xieydd@gmail.com>

--- docker/pg-cnpg/trunk-install.sh
+++ docker/pg-cnpg/trunk-install.sh
@@ -7,49 +7,49 @@ extension_count=$(<$file wc -l)
 lines=$(cat $file)
 for line in $lines
 do
-        output=$(trunk install $line 2>&1)
+    output=$(trunk install $line 2>&1)
 
-        if [ $? -ne 0 ]; then
-            if [[ $output == *"Failed to find an archive for"* || $output == *"Failed to fetch Trunk archive from"* ]]; then
-                version_not_found_extensions+=("$line")
-            else
-                echo "trunk install command failed"
-                trunk_install_failed_extensions+=("$line")
-            fi
+    if [ $? -ne 0 ]; then
+        if [[ $output == *"Failed to find an archive for"* || $output == *"Failed to fetch Trunk archive from"* ]]; then
+            version_not_found_extensions+=("$line")
+        else
+            echo "trunk install command failed"
+            trunk_install_failed_extensions+=("$line")
         fi
-        echo $output
-        printf "\n\n"
+    fi
+    echo $output
+    printf "\n\n"
 done
 IFS=$'\n' extensions=(`psql postgres://postgres:postgres@localhost:5432 -tA postgres -c 'select name from pg_available_extensions;'`)
 for ext in "${extensions[@]}"
 do
-        # drop schema columnar if ext name is columnar
-        if [ "$ext" == "columnar" ]; then
-            psql postgres://postgres:postgres@localhost:5432 -c "drop extension if exists citus_columnar cascade;"
+    # drop schema columnar if ext name is columnar
+    if [ "$ext" == "columnar" ]; then
+        psql postgres://postgres:postgres@localhost:5432 -c "drop extension if exists citus_columnar cascade;"
+    fi
+    # drop type semver if ext name is semver
+    if [ "$ext" == "semver" ]; then
+        psql postgres://postgres:postgres@localhost:5432 -c "drop extension if exists pg_text_semver cascade;"
+    fi
+    # if extension name is meta_triggers, create extension meta first and create extension meta_triggers
+    if [ "$ext" == "meta_triggers" ]; then
+        psql postgres://postgres:postgres@localhost:5432 -c "create extension if not exists hstore cascade;"
+        psql postgres://postgres:postgres@localhost:5432 -c "create extension if not exists meta cascade;"
+        psql postgres://postgres:postgres@localhost:5432 -c "create extension if not exists meta_triggers cascade;"
+    fi
+    output=$(psql postgres://postgres:postgres@localhost:5432 -c "create extension if not exists \"$ext\" cascade;" 2>&1)
+    if [ $? -ne 0 ]; then
+        if [[ $output == *"shared_preload_libraries"* ]]; then
+            need_load_shared_preload_libraries_extensions+=("$ext")
+        elif [[ $output == *"already exists"* ]]; then
+            echo "extension \"$ext\" already exists"
+        else
+            echo "CREATE EXTENSION command failed"
+            failed_extensions+=("$ext")
         fi
-        # drop type semver if ext name is semver
-        if [ "$ext" == "semver" ]; then
-            psql postgres://postgres:postgres@localhost:5432 -c "drop extension if exists pg_text_semver cascade;"
-        fi
-        # if extension name is meta_triggers, create extension meta first and create extension meta_triggers
-        if [ "$ext" == "meta_triggers" ]; then
-            psql postgres://postgres:postgres@localhost:5432 -c "create extension if not exists hstore cascade;"
-            psql postgres://postgres:postgres@localhost:5432 -c "create extension if not exists meta cascade;"
-            psql postgres://postgres:postgres@localhost:5432 -c "create extension if not exists meta_triggers cascade;"
-        fi
-        output=$(psql postgres://postgres:postgres@localhost:5432 -c "create extension if not exists \"$ext\" cascade;" 2>&1)
-        if [ $? -ne 0 ]; then
-            if [[ $output == *"shared_preload_libraries"* ]]; then
-                need_load_shared_preload_libraries_extensions+=("$ext")
-            elif [[ $output == *"already exists"* ]]; then
-                echo "extension \"$ext\" already exists"
-            else
-                echo "CREATE EXTENSION command failed"
-                failed_extensions+=("$ext")
-            fi
-        fi
-        echo $output
-        printf "\n\n"
+    fi
+    echo $output
+    printf "\n\n"
 done
 available_extensions_count=${#extensions[@]}
 failure_count=${#failed_extensions[@]}
@@ -70,11 +70,11 @@ echo "$need_load_shared_preload_libraries_count / $extension_count"
 printf "\n\n***NEED TO LOAD shared_preload_libraries EXTENSIONS***\n"
 for need in "${need_load_shared_preload_libraries_extensions[@]}"
 do
-      echo $need
+    echo $need
 done
 
 printf "\n\n***FAILED EXTENSIONS***\n"
 for failed in "${failed_extensions[@]}"
 do
-      echo $failed
+    echo $failed
 done
